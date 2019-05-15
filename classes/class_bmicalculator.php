77e2e28f812d3ab4bc03c675ea32864a8e6a0a14
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-05-15 11:14:13 +0200

    set methods

--- classes/class_bmicalculator.php
+++ classes/class_bmicalculator.php
@@ -1,12 +1,40 @@
 <?php
 
-class BmiCalculator {
-  // Dit zijn de classvariables die je ook wel Fields noemt
+class Person {
   var $firstname;
   var $infix;
   var $lastname;
-  var $bodymass;
-  var $bodylength; 
+
+  function make_full_name() {
+    return $this->firstname . " " . $this->infix . " " . $this->lastname;
+  }
+}
+
+class BmiCalculator extends Person {
+  // Dit zijn de classvariables die je ook wel Fields noemt  
+  private $bodylength; 
+  private $bodymass;
+
+  // Getter en setter methods
+  public function set_bodylength($bodylength) {
+    if ( $bodylength < 0.546 || $bodylength > 2.75 ) {
+      echo "Uw lengte is hoogstwaarschijnlijk niet correct opgegeven";
+      $this->bodylength = -1;
+    } else {
+      $this->bodylength = $bodylength;
+    }
+  }
+
+  public function set_bodymass($bodymass) {
+    if ( $bodymass < 2 || $bodymass > 750 ) {
+      echo "Uw massa is hoogstwaarschijnlijk niet correct opgegeven";
+      $this->bodymass = 0;
+    } else {
+      $this->bodymass = $bodymass;
+    }
+  }
+
+  
 
   // We gaan de constructor van de class maken. De constructor wordt altijd automagical aangeroepen wanneer we het woordje new gebruiken.
   function __construct($args = []) {
@@ -18,7 +46,7 @@ class BmiCalculator {
   }
 
   function welkom() {
-    echo "Hallo " . $this->firstname . " " . $this->infix . " " . $this->lastname . ", je massa is " . $this->bodymass . "kg en je lengte is: " . $this->bodylength . "m. Je BMI-waarde is: " . $this->calculate_bmi() . "<hr>";
+    echo "Hallo " . $this->make_full_name() . ", je massa is " . $this->bodymass . "kg en je lengte is: " . $this->bodylength . "m. Je BMI-waarde is: " . $this->calculate_bmi() . "<hr>";
   }
 
   // Maak een method die met de bodymass en bodylength de bmi berekent
--- index.php
+++ index.php
@@ -8,8 +8,8 @@
   $person1->firstname = "Arjan";
   $person1->infix = 'de';
   $person1->lastname = 'Ruijter';
-  $person1->bodylength = 1.8;
-  $person1->bodymass = 300;
+  $person1->set_bodylength(1.8);
+  $person1->set_bodymass(300);
 
   // Geef de class variable length weer op de pagina
   echo "Hallo " . $person1->firstname . ", je massa is " . $person1->bodymass . "kg en je lengte is: " . $person1->bodylength . "m<hr>";
