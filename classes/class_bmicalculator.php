93857c54221adb4d412ae7d744ba95a6d022199a
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-05-15 10:12:23 +0200

    class vars verandert

--- classes/class_bmicalculator.php
+++ classes/class_bmicalculator.php
@@ -2,19 +2,23 @@
 
 class BmiCalculator {
   // Dit zijn de classvariables die je ook wel Fields noemt
-  var $name;
+  var $firstname;
+  var $infix;
+  var $lastname;
   var $bodymass;
   var $bodylength; 
 
   // We gaan de constructor van de class maken. De constructor wordt altijd automagical aangeroepen wanneer we het woordje new gebruiken.
   function __construct($args = []) {
-    $this->name = $args['name'] ?? 'onbekend';
+    $this->firstname = $args['firstname'] ?? 'voornaam onbekend';
+    $this->infix = $args['infix'] ?? 'tussenvoegsel onbekend';
+    $this->lastname = $args['lastname'] ?? 'achternaam onbekend';
     $this->bodymass = $args['bodymass'] ?? 0;
     $this->bodylength = $args['bodylength'] ?? 1;
   }
 
   function welkom() {
-    echo "Hallo " . $this->name . ", je massa is " . $this->bodymass . "kg en je lengte is: " . $this->bodylength . "m. Je BMI-waarde is: " . $this->calculate_bmi() . "<hr>";
+    echo "Hallo " . $this->firstname . " " . $this->infix . " " . $this->lastname . ", je massa is " . $this->bodymass . "kg en je lengte is: " . $this->bodylength . "m. Je BMI-waarde is: " . $this->calculate_bmi() . "<hr>";
   }
 
   // Maak een method die met de bodymass en bodylength de bmi berekent
--- index.php
+++ index.php
@@ -5,20 +5,30 @@
   $person1 = new BmiCalculator();
 
   // Geef de class variabeles een waarde
-  $person1->name = "Arjan";
+  $person1->firstname = "Arjan";
+  $person1->infix = 'de';
+  $person1->lastname = 'Ruijter';
   $person1->bodylength = 1.8;
   $person1->bodymass = 300;
 
   // Geef de class variable length weer op de pagina
-  echo "Hallo " . $person1->name . ", je massa is " . $person1->bodymass . "kg en je lengte is: " . $person1->bodylength . "m<hr>";
-
+  echo "Hallo " . $person1->firstname . ", je massa is " . $person1->bodymass . "kg en je lengte is: " . $person1->bodylength . "m<hr>";
+  $person1->welkom();
   // Maak een nieuwe instantie van de class BmiCalculator
-  $person2 = new BmiCalculator(['name'=>'Evelien', 'bodymass'=>60, 'bodylength'=>1.65]);
+  $person2 = new BmiCalculator(['firstname'=>'Evelien',
+                                'infix'=>'van der',
+                                'lastname'=>'Veer',
+                                'bodymass'=>60, 
+                                'bodylength'=>1.65]);
 
   // Roep de welkom() method van de class BmiCalculator
   $person2->welkom();
 
-  $person3 = new BmiCalculator(['name'=>'Fred', 'bodymass'=>75, 'bodylength'=>1.8]);
+  $person3 = new BmiCalculator(['firstname'=>'Fred',
+                                'infix'=>'de',
+                                'lastname'=>'Beer',
+                                'bodymass'=>75, 
+                                'bodylength'=>1.8]);
   $person3->welkom();
   
 ?>
